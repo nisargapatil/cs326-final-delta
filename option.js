f5fb2ca6ac558f6738e89dacf1512a750bb254b6
Nisarga Patil <nisargapatil@1x-nat-vl931-172-30-163-64.wireless.umass.edu>
2021-10-23 22:32:33 -0400

    update

--- option.js
+++ option.js
@@ -5,6 +5,6 @@ function select(id){
 }
 
 
-document.getElementById("Op1").addEventListener('click', ()=>select("Op1"));
+document.getElementById("Op1").addEventListener('click', window.location = "Ratings.html");
 document.getElementById("Op2").addEventListener('click', ()=>select("Op2"));
 document.getElementById("Op3").addEventListener('click', ()=>select("Op3"));
