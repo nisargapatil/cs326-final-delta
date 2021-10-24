fdbcbd8f6528a0a4d09c2002d63e4d23591ca092
Nisarga Patil <nisargapatil@1x-nat-vl931-172-30-163-64.wireless.umass.edu>
2021-10-23 23:02:31 -0400

    redirecting to ratings page

--- option.js
+++ option.js
@@ -1,10 +1,19 @@
 'use strict';
 
 function select(id){
-    document.getElementById(id).classList.add("active");
+    // document.getElementById(id).classList.add("active");
+    if(id=="Op1"){
+        window.location = "foodRatePage.html";
+    }
+    if(id=="Op3"){
+        window.location = "entertainmentRatePage.html";
+    }
+    if(id=="Op2"){
+        window.location = "travelRatePage.html";
+    }
 }
 
 
-document.getElementById("Op1").addEventListener('click', window.location = "Ratings.html");
+document.getElementById("Op1").addEventListener('click', ()=>select("Op1"));
 document.getElementById("Op2").addEventListener('click', ()=>select("Op2"));
 document.getElementById("Op3").addEventListener('click', ()=>select("Op3"));
