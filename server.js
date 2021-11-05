777178ae0c72f7a1a5a5d00b008f7d0a7a44d9f7
Nisarga Patil <nisargapatil@1x-nat-vl931-172-30-143-90.wireless.umass.edu>
2021-11-05 19:27:28 -0400

    backend

--- server.js
+++ server.js
@@ -1,126 +1,73 @@
-// 'use strict';
-// let http = require('http');
-// let url = require('url');
-// let fs = require('fs');
-// const JSONfile = './data.json';
-// let product = {};
-// let user = {};
-
-// function addProduct(name, response) {
-//     product[name] = 0;
-//     fs.writeFileSync(JSONfile, JSON.stringify(product));
-//     response.write("<h1> counter " + name + " created </h1>");
-//     response.end();
-// }
-
-// function errorCounter(name, response) {
-//     response.write("<h1> error: counter " + name + " not found. </h1>");
-//     response.end();
-// }
-
-// function lookProduct(name, response) {
-//     reload(JSONfile);
-//     response.write("<h1> counter [" + name + "] = " + product[name] + " </h1>");
-// }
-
-// function updateProduct(name, response) {
-//     counter[name] += 1;
-//     fs.writeFileSync(JSONfile, JSON.stringify(product));
-//     response.write("<h1> counter " + name + " updated </h1>");
-// }
-
-// function deleteProduct(name, response) {
-//     delete product[name];
-//     fs.writeFileSync(JSONfile, JSON.stringify(product));
-//     response.write("<h1> counter " + name + " deleted </h1>");
-// }
-
-// function reload(filename) {
-//     if (fs.existsSync(filename)) {
-//         // READ IN COUNTER FROM THE FILE -- your code goes here
-//         product = JSON.parse(fs.readFileSync(filename));
-//     }
-//     else {
-//         product = {};
-//     }
-// }
-
-// function userSignup(name, response){
-//     user[name] = 0;
-//     fs.writeFileSync(JSONfile, JSON.stringify(name));
-//     response.write("<h1> counter " + name + " created </h1>");
-//     response.end();
-// }
-
-// function login(){
-
-// }
-
-
-// const headerText = { "Content-Type": "text/html" };
-// let server = http.createServer();
-// server.on('request', async (request, response) => {
-//     response.writeHead(200, headerText);
-//     let options = url.parse(request.url, true).query;
-//     response.write(JSON.stringify(options));
-//     if (request.url.startsWith("/product/add")) {
-//         addProduct(options.name, response);
-//         return;
-//     }
-//     if (!(options.name in counter)) {
-//         errorCounter(options.name, response);
-//         return;
-//     }
-//     if (request.url.startsWith("/product/Lookup")) {
-//         lookProduct(options.name, response);
-//     }
-//     else if (request.url.startsWith("/user/signup")) {
-//         updateCounter(options.name, response);
-//     }
-//     else if (request.url.startsWith("/delete")) {
-//         deleteCounter(options.name, response);
-//     }
-//     else {
-//         response.write("no command found.");
-//     }
-//     response.end();
-// });
-
-// server.listen(8080);
-
 'use strict';
-const express = require('express');
-const app = express();
+import { createServer } from 'http';
+import { parse } from 'url';
+import { writeFile, readFileSync, existsSync } from 'fs';
 
-app.use(express.json()); // lets you handle JSON input
+let database = {};
+let product_id = 0;
+let user_id = 0;
 
-const port = 3000;
+if (existsSync("database.json")) {
+    database = JSON.parse(readFileSync("database.json"));
+} else {
+    database = {
+        users: [],
+        products: [],
+        pages: ['food', 'travel', 'entertainment']
+    };
+}
 
-const datastore = {};
+createServer(async (req, res) => {
+    const parsed = parse(req.url, true);
 
-app.get('/product/add', (req, res) => {
-  res.send('Hello World!')
-});
+    if (parsed.pathname === '/createUser') {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            user_id += 1
+            database.users.push({
+                name: data.name,
+                password: data.password,
+                email: data.email,
+                id: user_id
+            });
 
-app.get('/product/Lookup', (req, res) => {
-  res.send('Set.');
-});
+            writeFile("database.json", JSON.stringify(database), err => {
+                if (err) {
+                    console.err(err);
+                } else res.end();
+            });
+            res.writeHead(200);
+            res.write("User created");
+        });
+    }
+    else if (parsed.pathname === '/addProduct') {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            product_id += 1
+            database.products.push({
+                name: data.name,
+                category: data.category,
+                description: "null",
+                details: data.details,
+                id: product_id
+            });
 
-app.get('/read', (req, res) => {
-  res.send(`key = ${k}, value = ${v}`);
-});
+            writeFile("database.json", JSON.stringify(database), err => {
+                if (err) {
+                    console.err(err);
+                } else res.end();
+            });
+            res.writeHead(200);
+            res.write("Product added");
+        });
+    }
+    else {
+        res.writeHead(404);
+        res.end();
+    }
+}).listen(8080);
 
-//   curl -d '{ "value" : "12" }' -H "Content-Type: application/json" http://localhost:3000/read/x
-app.get('/read/:key', (req, res) => {
-  res.send();
-});
-
-//   curl -d '{ "key" : "x", "value" : "12" }' -H "Content-Type: application/json" http://localhost:3000/pcreate
-app.post('/pcreate', (req, res) => {
-  res.send('Set.');
-});
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
