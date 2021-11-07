b64b75e561bc0ea39e7c66fd42836966165e0344
Elizabeth Seto <elizabethseto@Elizabeths-Air.fios-router.home>
2021-11-07 16:27:14 -0500

    entertainment/travel rendering

--- client/wevote.js
+++ client/wevote.js
@@ -55,6 +55,7 @@ async function render() {
                 let param = {};
                 let url = '/productInfo';
                 param['name'] = name;
+                name = name.replace(/%20/g, " ");
                 let res = await getJSON(url, param);
                 let json = await res.text();
                 let obj = JSON.parse(json);
@@ -63,6 +64,130 @@ async function render() {
                 let htmlSegment = `<div class="itemCard">
                 <h1>${obj.name}</h1>
                 <img src="./imgs/foodPic.jpg" width="200" height="200"><br><br>
+                <button type="button" class="btn btn-dark">Upvote Number</button>
+                <button type="button" class="btn btn-dark">Downvote Number</button>
+                </div>`;
+
+                html += htmlSegment;
+
+                htmlSegment = `<div class="itemCard">
+                    <h3>Food Description</h3>
+                    <span id="productDescription"></span>
+                    ${obj.description}
+                </div>`;
+
+                html += htmlSegment;
+
+                htmlSegment = `<div class="itemCard">
+                                <h3>Other Details</h3>
+                        <span id="otherDetails"></span>
+                        </div>`;
+
+                html += htmlSegment;
+
+                container2.innerHTML = html;
+            }
+        }
+        else if (page === 'travelRatePage') {
+            let url = '/productSummary';
+            let res = await getJSON(url, {category: 'Travel'});
+            let json = await res.text();
+            let obj = JSON.parse(json);
+            let container2 = document.getElementById('container2');
+            let html = '';
+            for (let i=0; i < obj.length; i++) {
+                let htmlSegment = `<div class="itemCard">
+                <h1>${obj[i].name}</h1>
+                <img src="./imgs/travelPic.jpg" width="200" height="200"><br><br>
+                <button type="button" class="btn btn-dark">Upvote</button>
+                <button type="button" class="btn btn-dark">Downvote</button>
+                <a href="viewPage?page=travelProductPage&name=${obj[i].name}">
+                    <button type="button" button type="submit" class="btn btn-dark" href="#">View</button>
+                </a>
+                </div>`;
+                html += htmlSegment;
+            }
+            container2.innerHTML = html;
+        }
+        else if (page === 'travelProductPage') {
+            let nameIndex = window.location.href.indexOf("name=");
+        
+            if (nameIndex > 0) {
+                let name = window.location.href.substr(nameIndex + 5);
+                let param = {};
+                let url = '/productInfo';
+                param['name'] = name;
+                name = name.replace(/%20/g, " ");
+                let res = await getJSON(url, param);
+                let json = await res.text();
+                let obj = JSON.parse(json);
+                let container2 = document.getElementById('container2');
+                let html = '';
+                let htmlSegment = `<div class="itemCard">
+                <h1>${obj.name}</h1>
+                <img src="./imgs/travelPic.jpg" width="200" height="200"><br><br>
+                <button type="button" class="btn btn-dark">Upvote Number</button>
+                <button type="button" class="btn btn-dark">Downvote Number</button>
+                </div>`;
+
+                html += htmlSegment;
+
+                htmlSegment = `<div class="itemCard">
+                    <h3>Food Description</h3>
+                    <span id="productDescription"></span>
+                    ${obj.description}
+                </div>`;
+
+                html += htmlSegment;
+
+                htmlSegment = `<div class="itemCard">
+                                <h3>Other Details</h3>
+                        <span id="otherDetails"></span>
+                        </div>`;
+
+                html += htmlSegment;                 
+
+                container2.innerHTML = html;
+            }
+        }
+        else if (page === 'entertainmentRatePage') {
+            let url = '/productSummary';
+            let res = await getJSON(url, {category: 'Entertainment'});
+            let json = await res.text();
+            let obj = JSON.parse(json);
+            let container2 = document.getElementById('container2');
+            let html = '';
+            for (let i=0; i < obj.length; i++) {
+                let htmlSegment = `<div class="itemCard">
+                <h1>${obj[i].name}</h1>
+                <img src="./imgs/movie.jpg" width="200" height="200"><br><br>
+                <button type="button" class="btn btn-dark">Upvote</button>
+                <button type="button" class="btn btn-dark">Downvote</button>
+                <a href="viewPage?page=entertainmentProductPage&name=${obj[i].name}">
+                    <button type="button" button type="submit" class="btn btn-dark" href="#">View</button>
+                </a>
+                </div>`;
+                html += htmlSegment;
+            }
+            container2.innerHTML = html;
+        }
+        else if (page === 'entertainmentProductPage') {
+            let nameIndex = window.location.href.indexOf("name=");
+        
+            if (nameIndex > 0) {
+                let name = window.location.href.substr(nameIndex + 5);
+                let param = {};
+                let url = '/productInfo';
+                name = name.replace(/%20/g, " ");
+                param['name'] = name;
+                let res = await getJSON(url, param);
+                let json = await res.text();
+                let obj = JSON.parse(json);
+                let container2 = document.getElementById('container2');
+                let html = '';
+                let htmlSegment = `<div class="itemCard">
+                <h1>${obj.name}</h1>
+                <img src="./imgs/movie.jpg" width="200" height="200"><br><br>
                 <button type="button" class="btn btn-dark">Upvote Number</button>
                 <button type="button" class="btn btn-dark">Downvote Number</button>
                 </div>`;
