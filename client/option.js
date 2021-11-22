50a3fdf4ad6543b773a38bb22190a01cdfd0f4c3
Elizabeth Seto <elizabethseto@1x-nat-vl932-172-30-192-34.wireless.umass.edu>
2021-11-22 09:35:47 -0500

    log in

--- client/option.js
+++ client/option.js
@@ -13,8 +13,17 @@ function select(id){
     }
 }
 
+function please_login() {
+    alert('Please login or create an account first!');
+}
+
 if (isLogin()) {
     document.getElementById("Op1").addEventListener('click', ()=>select("Op1"));
     document.getElementById("Op2").addEventListener('click', ()=>select("Op2"));
     document.getElementById("Op3").addEventListener('click', ()=>select("Op3"));    
+}
+else {
+    document.getElementById("Op1").addEventListener('click', ()=>please_login());
+    document.getElementById("Op2").addEventListener('click', ()=>please_login());
+    document.getElementById("Op3").addEventListener('click', ()=>please_login());    
 }
